8babc555b513de43c96660674fc569401386bc05
zoujinqiang_glob <2339311136@qq.com>
2022-09-23 20:16:16 +0800

    修改了基础工程

--- BasicCourse/INC/STC51.h
+++ BasicCourse/INC/STC51.h
@@ -2,6 +2,8 @@
 #define STC51_H
 //1为天翔单片机
 //0普中单片机
+#define CLOSE  0
+#define OPEN   1
 #define STC51   1
 #if STC51
 #define _C51    1
@@ -9,14 +11,14 @@
 #define _C51    2
 #endif
 //对应为1 打开相应外设（串口不能用）
-#define TIME_   0//26
-#define USART   0   //52
-#define BEEP_   0   //18
-#define KEY_    0  //6
-#define SMG_    1   //19
-#define LED_     0  //22
-#define DS18B20_ 0
-#define LCD1602_ 1
+#define TIME_      CLOSE//26
+#define USART      CLOSE //52
+#define BEEP_      CLOSE //18
+#define KEY_       CLOSE  //6
+#define SMG_       OPEN   //19
+#define LED_       CLOSE  //22
+#define DS18B20_   CLOSE
+#define LCD1602_   OPEN
 
 typedef unsigned int uint16;
 typedef unsigned char uint8;
